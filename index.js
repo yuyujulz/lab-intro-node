d06ebc4de85b605888e33a956163948c3ee10f44
Julia Ganina <ganina.julia2@gmail.com>
2022-12-06 22:09:21 +0000

    9/14 done

--- index.js
+++ index.js
@@ -1,17 +1,59 @@
+
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a,b) => a - b)
+  }
 
-  get(pos) {}
+  get(pos) {
+    let position = this.items[pos]
+    if(position === undefined){
+      throw new Error('OutOfBounds');
 
-  max() {}
+    }
+    return position
+  }
+    
 
-  min() {}
+  max() {
+    
+    const maximum = Math.max(this.items);
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    return maximum
+  }
 
-  sum() {}
+  min() {
+    
+    const minimum = Math.min(this.items);
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    return minimum
+  }
 
-  avg() {}
+  sum() {
+    if(this.length === 0){
+      return 0;
+    }
+    for(let i = 0; i < this.length; i++){
+      sum += this.items[i]
+    }
+      return sum
+    }
+  
+
+  avg() {
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    }
+  }
 }
 
 module.exports = SortedList;
